b02c148ca8d58f44acbbc67e4704f492409fa024
kohye <kohyechyang11@gmail.com>
2024-04-19 16:07:00 +0800

    completed Practical 2

---
+++ Week 1 Practical 2/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+});
+
+app.post('/', (req,res) => {
+    res.send('Got a POST request')
+});
+
+app.put('/user', (req,res) => {
+    res.send('Got a PUT request at /user')
+});
+
+app.delete('/user', (req,res) => {
+    res.send('Got a DELETE request at /user')
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
